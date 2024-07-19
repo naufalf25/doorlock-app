771414cee09833b52b9ef735ba79c88fc7b568e1
Muhammad Naufal Farras <opal.farras@gmail.com>
2024-07-19 07:10:44 +0700

    Add manifest.json with sw

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from 'next/constants.js';
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -10,4 +16,14 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import('@ducanh2912/next-pwa')).default({
+      dest: 'public',
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default nextConfigFunction;
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,30 @@ import NextTopLoader from 'nextjs-toploader';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Kunci Pintuku',
+  applicationName: 'Kunci Pintuku App',
+  title: 'Kunci Pintuku App',
+  description: 'Aplikasi website kunci pintu ESP32',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Kunci Pintuku App',
+  },
+  formatDetection: {
+    telephone: false,
+  },
+};
+
+export const viewport = {
+  themeColor: '#2E236C',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+      </head>
       <body className={inter.className}>
         <NextTopLoader />
         {children}

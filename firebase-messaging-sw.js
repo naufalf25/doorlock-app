207bf6da57be4bcdb828bd9566a717897a519f16
Muhammad Naufal Farras <opal.farras@gmail.com>
2024-07-19 13:31:25 +0700

    Update try raw config firebase at sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
-console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
